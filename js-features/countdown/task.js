5094aceda03770cf8c7edc68226e72bf2a244186
Kirill <kiril-an@mail.ru>
2025-07-13 14:39:48 +0300

    первое задание

---
+++ js-features/countdown/task.js
@@ -0,0 +1,13 @@
+const timerElement = document.getElementById('timer');
+let secondsLeft = parseInt(timerElement.textContent);
+
+function updateTimer() {
+    secondsLeft--;
+    timerElement.textContent = secondsLeft;
+    if (secondsLeft <= 0) {
+        clearInterval(interval);
+        alert('Вы победили в конкурсе!');
+    }
+}
+
+let interval = setInterval(updateTimer, 1000);
